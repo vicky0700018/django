102f50448bc7b696d34b0d4a9542ab7b71154f98
vicky0700018 <vs2734514@gmail.com>
2026-04-14 22:11:42 +0530

    updated

--- myproject/google/urls.py
+++ myproject/google/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
-
+from django.urls import path
+from .views import * ## Importing all views from the current app's views.py file
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/',include('google.urls'))
+    path('categorie', category_products, name='category_products'),
 ]
-
-
-urlpatterns += static( settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)
