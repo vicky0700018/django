46625b4b37ea975a730c0d2fcc759428663450f2
vicky0700018 <vs2734514@gmail.com>
2026-04-14 22:11:22 +0530

    updated

--- myproject/google/models.py
+++ myproject/google/models.py
@@ -7,9 +7,21 @@
 #1. python manage.py makemigrations
 #2. python manage.py migrate
 
+
 from django.db import models
 
+# Category Model
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    category_image = models.ImageField(upload_to='categories/', null=True, blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+# Product Model
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     product_name = models.CharField(max_length=255)
     product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_description = models.TextField()
@@ -17,3 +29,4 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
