568b3f5893b660a5474ca2d1d7977c3d1dc41372
vicky0700018 <vs2734514@gmail.com>
2026-04-16 21:17:07 +0530

    updated

--- myproject/google/views.py
+++ myproject/google/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import *  ## Importing all models from the current app's models.py file
 
-# Create your views here.
+def category_products(request):
+    categories = Category.objects.prefetch_related('products').all()
+    return render(request, 'category_products.html', {'categories': categories})
