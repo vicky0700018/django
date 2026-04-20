5e78d6d5514911abfd54bd3d86c8c3c5d296bb47
vicky0700018 <vs2734514@gmail.com>
2026-04-20 23:12:56 +0530

    updated

--- myproject/google/urls.py
+++ myproject/google/urls.py
@@ -22,4 +22,5 @@ from .views import * ## Importing all views from the current app's views.py file
 
 urlpatterns = [
     path('categorie', category_products, name='category_products'),
+    path('students', students_list, name='students_list'),
 ]
