cc5a7bc4d33533e645f3fc76ea8865a78766b12b
vicky0700018 <vs2734514@gmail.com>
2026-04-20 23:13:20 +0530

    updated

--- myproject/google/views.py
+++ myproject/google/views.py
@@ -4,3 +4,7 @@ from .models import *  ## Importing all models from the current app's models.py 
 def category_products(request):
     categories = Category.objects.prefetch_related('products').all()
     return render(request, 'category_products.html', {'categories': categories})
+
+def students_list(request):
+    students = Student.objects.all()
+    return render(request, 'students_list.html', {'students': students})
